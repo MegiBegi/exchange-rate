a6821797f2a44cbb29ae061ecd34db997530e2d4
MegiBegi <ms.magdalena.kozlowska@gmail.com>
2021-07-22 20:50:56 +0200

    Fetch real data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,18 @@ import type { AppProps } from "next/app";
 import { ChakraProvider, Container } from "@chakra-ui/react";
 
 import { appWithTranslation } from "next-i18next";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Container maxW="100%" p={0} boxSizing="border-box">
-        <Component {...pageProps} />
-      </Container>
+      <QueryClientProvider client={queryClient}>
+        <Container maxW="100%" p={0} boxSizing="border-box">
+          <Component {...pageProps} />
+        </Container>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
